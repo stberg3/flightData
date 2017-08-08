308c8934d5a7f67f8d1afd4834db3b4ea035d77e
stberg3 <stberg3@gmail.com>
2017-08-08 15:48:22 -0500

    1st commit, yo

---
+++ spirit_curl.sh
@@ -0,0 +1,37 @@
+curl 'https://www.google-analytics.com/collect'
+	-H 'origin: https://www.spirit.com'
+	-H 'accept-encoding: gzip, deflate, br'
+	-H 'accept-language: en-US,en;q=0.8'
+	-H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+	-H 'content-type: text/plain;charset=UTF-8'
+	-H 'accept: */*'
+	-H 'cache-control: max-age=0'
+	-H 'authority: www.google-analytics.com'
+	-H 'referer: https://www.spirit.com/Default.aspx' --data-binary
+	'v=1&
+	_v=j58&
+	a=1022332392&
+	t=event&
+	_s=1&
+	dl=https%3A%2F%2Fwww.spirit.com%2FDefault.aspx&
+	ul=en-us&
+	de=UTF-8&
+	dt=Spirit%20Airlines%20%7C%20flights%2C%20check-in%2C%20boarding%20pass%2C%20flight%20status%2C%20vacations&
+	sd=24-bit&
+	sr=1920x1080&
+	vp=1840x966&
+	je=0&
+	ec=Flight%20Search&
+	ea=MSP-ATL&
+	el=Homepage&
+	_u=SCCACEArJ~&
+	jid=&
+	gjid=&
+	cid=800234400.1502128704&
+	tid=UA-206943-1&
+	_gid=156465755.1502128704&
+	gtm=GTM-KLL8ZL&
+	cd1=guest&
+	linkid=book-travel-form&
+  z=1465937029' 
+  --compressed
